ecbb5419a44cf1ceddd364e21f3c04d4c526e0ea
Rafael Verediano <rverediano@gmail.com>
2025-10-02 21:32:23 -0300

    With new environment

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ test_msg: str = 'test message to the user'
 if not test_msg:
     print('no message defined')
 else:
-    print(test_msg)
+    print(test_msg.lower())
+print(test_msg.upper())
+print(test_msg.capitalize())
